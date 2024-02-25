5c31fb490b2537997b899bae31eae6aed6ae0064
Moteeullah Azmi <you@example.com>
2024-02-25 11:32:41 +0530

    Azmi

---
+++ script.js
@@ -0,0 +1,22 @@
+const input = document.getElementById("input");
+const button =document.querySelectorAll("button");
+
+button.forEach(element=>{
+    element.addEventListener("click",(e)=>{
+        console.log(e.target.textContent);
+
+        if(e.target.textContent==="C"){
+            input.innerText="";
+        }
+        else if(e.target.textContent==="<"){
+            input.innerText=input.innerText.slice(0,-1)
+        }
+        else if(e.target.textContent === "="){
+            input.innerText = eval(input.innerText)
+        }
+        else{
+            input.innerText+= e.target.textContent
+        }
+        input.scrollLeft = input.scrollWidth;
+    })
+})
